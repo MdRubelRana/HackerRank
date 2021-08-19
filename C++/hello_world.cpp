dae0632cda9ec9f158ed07a5c7f4c5e751895790
Md. Rubel Rana <rubel.rana@northsouth.edu>
2021-08-19 11:24:02 +0600

    input_and_output -> c++

--- C++/hello_world.cpp
+++ C++/hello_world.cpp
@@ -1,11 +1,11 @@
 
 // Problem link: https://www.hackerrank.com/challenges/cpp-hello-world/problem
 
-#include <iostream>
-#include <cstdio>
-using namespace std;
+ #include <iostream>
+ #include <cstdio>
+ using namespace std;
 
-int main() {
-    printf("Hello, World!");
-    return 0;
-}
+ int main() {
+     printf("Hello, World!");
+     return 0;
+ }
---
+++ C++/input_and_output.cpp
@@ -0,0 +1,16 @@
+
+// Problem link: https://www.hackerrank.com/challenges/cpp-input-and-output/problem
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    int a,b,c;
+    int sum=0;
+    cin >> a >> b >> c;
+    sum=a+b+c;
+    cout << sum << endl;
+    return 0;
+}
