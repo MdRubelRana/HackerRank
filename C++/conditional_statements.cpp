139bec9d0db95061435b382e6b00cb2dcfe529cf
Md. Rubel Rana <rubel.rana@northsouth.edu>
2021-08-19 11:35:03 +0600

    conditional_statements -> c++

---
+++ C++/conditional_statements.cpp
@@ -0,0 +1,43 @@
+
+// Problem link: https://www.hackerrank.com/challenges/c-tutorial-conditional-if-else/problem
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int number;
+    cin >> number;
+
+    if(number == 1){
+        cout << "one" << endl;
+    }
+    else if(number == 2){
+        cout << "two" << endl;
+    }
+    else if(number == 3){
+        cout << "three" << endl;
+    }
+    else if(number == 4){
+        cout << "four" << endl;
+    }
+    else if(number == 5){
+        cout << "five" << endl;
+    }
+    else if(number == 6){
+        cout << "six" << endl;
+    }
+    else if(number == 7){
+        cout << "seven" << endl;
+    }
+    else if(number == 8){
+        cout << "eight" << endl;
+    }
+    else if(number == 9){
+        cout << "nine" << endl;
+    }
+    else{
+        cout << "Greater than 9" << endl;
+    }
+    return 0;
+}
